863a061fb2079a6a910e3abe0af37f1fb5fe8cd0
Andrew Rea <email@andrewrea.co.uk>
2017-10-20 10:12:36 +0000

    Asserting on the first event from creating a new object

--- test/something.js
+++ test/something.js
@@ -1,5 +1,37 @@
+require('should');
+
+function TodoList(id){
+
+    let uncommittedEvents = [];
+    let self = {};
+
+    if (id === undefined){
+        uncommittedEvents.push(new TodoListCreated());
+    }
+
+    self.uncommittedEvents = () => {
+        return uncommittedEvents;
+    }
+
+    self.add = (todo) => {
+    };
+
+    return self;
+}
+
+function Todo(){
+
+}
+
+function TodoListCreated(){
+
+}
+
 describe('something', () => {
     it('does something', () => {
+        var todoList  = new TodoList();
+        //todoList.add(new Todo('Get the milk'));
 
+        todoList.uncommittedEvents().length.should.equal(1);
     });
 });
